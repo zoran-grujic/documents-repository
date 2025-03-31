606947601b6ea947013919ad6935d28e487f4dce
Zoran Grujic <zoran.grujic@gmail.com>
2025-03-31 13:52:06 +0200

    error reporting

--- config/web.php
+++ config/web.php
@@ -24,7 +24,7 @@ $config = [
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => 'site/error', // Define the action to handle errors
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
